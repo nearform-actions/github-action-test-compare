e317d52631822ed4ac79341e11e1c334ccd8e874
Ian Lovell <ian.lovell@nearform.com>
2023-03-22 17:50:28 +0000

    chore: add initial

---
+++ tests/missing-tests.test.ts
@@ -0,0 +1,40 @@
+import { createMockGitHub, MockGitHub, failedStep, successStep } from './utils'
+
+describe('github-action-test-compare', () => {
+  let mockGitHub: MockGitHub
+
+  beforeEach(async () => {
+    mockGitHub = createMockGitHub()
+    await mockGitHub.setup()
+  })
+
+  afterEach(async () => {
+    await mockGitHub.teardown()
+  })
+
+  it('should return error for no tests folder available', async () => {
+    const act = await mockGitHub.act()
+    const result = await act
+      .setEvent({
+        pull_request: {
+          head: {
+            ref: 'pr'
+          },
+          base: {
+            ref: 'main'
+          }
+        }
+      })
+      .runEvent('pull_request')
+
+    expect(result).toMatchObject([
+      successStep('Main Checkout'),
+      failedStep('Main Test compare'),
+      failedStep(
+        'Main Copy tests',
+        `cp: cannot stat '__tests__': No such file or directory`
+      ),
+      successStep('Post Test compare')
+    ])
+  })
+})
---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vitest/config";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    testTimeout: 120000,
+  },
+  plugins: [viteTsconfigPaths()],
+});
