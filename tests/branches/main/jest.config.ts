6b7d63ca97b87c225a8fdc737d98d11db76f35ae
Github <ian.lovell@nearform.com>
2023-03-28 19:00:08 +0100

    test: update to jest

---
+++ tests/branches/main/jest.config.ts
@@ -0,0 +1,7 @@
+import type { Config } from '@jest/types';
+
+const jestConfig: Config.InitialOptions = {
+  testTimeout: 120000,
+};
+
+export default jestConfig;
--- tests/branches/main/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-
-export default defineConfig({
-  test: {
-    globals: true,
-  },
-  plugins: [viteTsconfigPaths()],
-});
