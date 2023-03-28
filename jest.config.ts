1528251c68ec7caa520707a9b82fcf7bd0deab8d
Github <ian.lovell@nearform.com>
2023-03-28 18:31:31 +0100

    build: update names

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from '@jest/types';
-
-const jestConfig: Config.InitialOptions = {
-  modulePathIgnorePatterns: ['<rootDir>/tests/branches'],
-  testTimeout: 120000,
-};
-
-export default jestConfig;
